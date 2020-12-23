92e37a17f36853dfb957596537128888734f2db1
schwarzwaldsvd <svartskog@gmail.com>
2020-12-23 17:52:50 -0500

    Added Introduction and Features

---
+++ Features/Features/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x*x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(square(add(3,5)));
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { id = 1, Name = "Scott"},
+                new Employee { id = 2, Name = "Chris"}
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee { id = 3, Name = "Alex"}
+            };
+
+            //foreach (var person in developers)
+            //{
+            //    Console.WriteLine(person.Name);
+
+            //}
+            //foreach (var person in sales)
+            //{
+            //    Console.WriteLine(person.Name);
+
+            //}
+
+            //Console.WriteLine(developers.Count());
+            //IEnumerator<Employee> enumerator = sales.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+
+            //var query = developers.Where(e => e.Name.Length == 5)
+            //    .OrderBy(e => e.Name).Select(e => e);
+            
+            var query = developers.Where(e => e.Name.Length == 5)
+                .OrderByDescending(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name descending
+                         select developer;
+
+            foreach (var employee in query2)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Introduction/Introduction/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intriduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("- - - - - - - - ");
+            ShowLargeFilesWithLinq1(path);
+            Console.WriteLine("- - - - - - - - ");
+            ShowLargeFilesWithLinq2(path);
+            Console.ReadKey();
+        }
+
+        private static void ShowLargeFilesWithLinq1(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithLinq2(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i<5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+    }
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
